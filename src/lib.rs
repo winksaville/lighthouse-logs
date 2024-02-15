1aae864496469e632f852a65c64b5275107c6db8
Wink Saville <wink@saville.com>
2024-02-15 12:51:51 -0800

    feat: Add ReadTruncatedLines
    Also added a simple `test_read_truncated_lines`. This is more
    general to use than `process_line`.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,155 @@ use log::debug;
 
 pub trait MyTrait: std::io::Read + Seek {}
 
+pub struct ReadTruncatedLines<R: Read> {
+    reader: BufReader<R>,
+    reader_id: String,
+    capacity: usize,
+    max_line_length: u64,
+    full_line_len: usize,
+    line: String,
+    discard_line: String,
+    line_number: usize,
+    max_processed_full_line_len: usize,
+}
+
+impl<R: Read> ReadTruncatedLines<R> {
+    pub fn new(
+        reader: R,
+        reader_id_like_file_name: &str,
+        capacity: usize,
+        max_line_length: u64,
+    ) -> Self {
+        ReadTruncatedLines {
+            reader: BufReader::with_capacity(capacity, reader),
+            reader_id: reader_id_like_file_name.to_string(),
+            capacity,
+            max_line_length,
+            full_line_len: 0,
+            line: String::with_capacity(max_line_length as usize),
+            discard_line: String::with_capacity(max_line_length as usize),
+            line_number: 0,
+            max_processed_full_line_len: 0,
+        }
+    }
+
+    pub fn read_truncated_line(&mut self) -> Option<&str> {
+        //<ReadTruncatedLines<R> as IntoIterator>::Item> {
+        self.line.clear();
+        match self
+            .reader
+            .by_ref()
+            .take(self.max_line_length)
+            .read_line(&mut self.line)
+        {
+            Ok(orig_len) => {
+                let mut too_long = false;
+                if orig_len == 0 {
+                    // If we read 0 bytes, we are at EOF
+                    debug!("{}: EOF", self.line_number);
+                    return None;
+                } else if self.line.ends_with("\n") {
+                    // We have a complete line, remove the LF
+                    self.line.pop();
+                } else {
+                    // It might be too long or it's the last line and there is no-lf.
+                    // Either way it will be handled properly in too_long loop.
+                    debug!("Line {} is too long", self.line_number);
+                    too_long = true;
+                }
+
+                self.line_number += 1;
+                self.full_line_len = self.line.len();
+                debug!("{}, line_len={}", self.line_number, self.line.len());
+
+                if too_long {
+                    // Loop until we find the end of the line, ignoring the rest
+                    let mut ignore_loops = 0;
+                    loop {
+                        self.discard_line.clear();
+
+                        match self
+                            .reader
+                            .by_ref()
+                            .take(self.max_line_length)
+                            .read_line(&mut self.discard_line)
+                        {
+                            Ok(len) => {
+                                // Update current line length and max_processed_line_len
+
+                                if len == 0 {
+                                    debug!("ignore_loop: {ignore_loops}: EOF ignoring, full_line_len={}", self.full_line_len);
+                                    break;
+                                } else if self.line.ends_with("\n") {
+                                    debug!("ignore_loop: {ignore_loops}: LF end of line ignoring, full_line_len={}", self.full_line_len);
+                                    self.line.pop();
+                                    self.full_line_len += self.line.len();
+                                    break;
+                                } else {
+                                    self.full_line_len += len;
+                                    debug!("ignore_loop: {ignore_loops}: line_number={}, ignoring len={} full_line_len={}", self.line_number, len, self.full_line_len);
+                                }
+                            }
+                            Err(e) => {
+                                debug!(
+                                    "ignore_loop: {ignore_loops}: error reading \"{}\": {e}",
+                                    self.reader_id
+                                );
+
+                                // TODO, we should probably return the line count and max line length
+                                return None;
+                            }
+                        }
+                        ignore_loops += 1;
+                    }
+                }
+
+                debug!(
+                    "{}, line_len={} full_line_len={}",
+                    self.line_number,
+                    self.line.len(),
+                    self.full_line_len
+                );
+
+                // Remember the longest line we've processed
+                if self.full_line_len > self.max_processed_full_line_len {
+                    self.max_processed_full_line_len = self.full_line_len;
+                }
+
+                Some(&self.line)
+            }
+            Err(e) => {
+                debug!("Error reading \"{}\": {e}", self.reader_id);
+                None
+            }
+        }
+    }
+
+    pub fn line_number(&self) -> usize {
+        self.line_number
+    }
+
+    pub fn max_processed_full_line_len(&self) -> usize {
+        self.max_processed_full_line_len
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+
+    pub fn line(&self) -> &str {
+        &self.line
+    }
+
+    pub fn line_len(&self) -> usize {
+        self.line.len()
+    }
+
+    pub fn full_line_len(&self) -> usize {
+        self.full_line_len
+    }
+}
+
 // The nominal mininal line length is 1 because it ends with a LF (0x0A)
 // returns 0 if EOF.
 pub fn process_line(line_number: usize, line: &str) -> usize {
@@ -133,6 +282,29 @@ mod test {
     fn test_error_and_panic() {
         error!("Error prior to panic");
         panic!();
+    }
+
+    #[test]
+    fn test_read_truncated_lines() {
+        let expected_lines = vec!["line 1", "line 2", "", "last line 4"];
+        let fname = "data/log.txt";
+        let f = match File::open(fname) {
+            Ok(fr) => fr,
+            Err(e) => {
+                error!("Could not open \"{fname}\": {e}");
+                panic!();
+            }
+        };
+        let mut reader = BufReader::new(f);
+        let mut rtl = ReadTruncatedLines::new(&mut reader, fname, 1024, 1024);
+
+        for expected_line in expected_lines.into_iter() {
+            let line = rtl.read_truncated_line().unwrap();
+            println!("{line}");
+            assert_eq!(line, expected_line);
+        }
+        assert_eq!(rtl.line_number(), 4);
+        assert_eq!(rtl.max_processed_full_line_len(), 11);
     }
 
     #[test]
